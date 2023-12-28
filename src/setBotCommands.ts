cb6578209d16343391cac0a59f0033863061506b
EricStrohmaier <eric.strohmaier00@gmail.com>
2023-12-28 18:24:46 +0100

    first commit

--- src/setBotCommands.ts
+++ src/setBotCommands.ts
@@ -1,53 +1,15 @@
-// import {  userConfig } from './main';
 import { TRANSLATIONS } from './translation';
-import { PARAMETERS } from './parameters';
 
 export function setBotCommands(
     bot: { setMyCommands: (arg0: { command: string; description: string; }[]) => void; },
-    // chatId?: string
-) {
-    // const userConfigs = getUserConfigs();
-    // const language = userConfigs[chatId || '']?.language|| PARAMETERS.LANGUAGE;    
-    const language = PARAMETERS.LANGUAGE;    
+) { 
     bot.setMyCommands([
         {
             command: 'start',
             description:
-      TRANSLATIONS[language][
+      TRANSLATIONS['en'][
           'command-descriptions'
       ].start,
-        },
-        {
-            command: 'jobs',
-            description: TRANSLATIONS[language][
-                'command-descriptions' 
-            ].jobs,
-        },
-        {
-            command: 'jobalert',
-            description: TRANSLATIONS[language][
-                'command-descriptions' 
-            ].jobalert,
-        },
-        {
-            command: 'value4value',
-            description: TRANSLATIONS[language][
-                'command-descriptions' 
-            ].donate,
-        },
-        // {
-        //     command: 'privacy',
-        //     description:
-        //   TRANSLATIONS[language][
-        //       'command-descriptions'
-        //   ].privacy,
-        // },
-        {
-            command: 'freeguide',
-            description: TRANSLATIONS[language][
-                'command-descriptions' 
-            ].freeguide,
-
         },
     ]);
 }
